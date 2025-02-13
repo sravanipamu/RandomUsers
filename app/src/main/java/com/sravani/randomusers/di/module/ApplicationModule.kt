e70ca5c36a327b9648c3116bad317aa16a55d763
sravanip <sravanip@yupptv.com>
2025-02-13 14:38:00 +0530

    added dependency injection using hilt

---
+++ app/src/main/java/com/sravani/randomusers/di/module/ApplicationModule.kt
@@ -0,0 +1,36 @@
+package com.sravani.randomusers.di.module
+
+import com.sravani.randomusers.data.api.NetworkService
+import com.sravani.randomusers.di.BaseUrl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule {
+    @BaseUrl
+    @Provides
+    fun provideBaseUrl(): String = "https://randomuser.me/api/"
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideNetworkService(
+        @BaseUrl baseUrl: String, gsonConverterFactory: GsonConverterFactory
+    ): NetworkService {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+            .create(NetworkService::class.java)
+    }
+
+
+}
---
+++ app/src/main/java/com/sravani/randomusers/di/qualifiers.kt
@@ -0,0 +1,7 @@
+package com.sravani.randomusers.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
