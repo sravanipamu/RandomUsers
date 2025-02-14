f2a30c59c0523646564e9093e9f757d2c76a08ca
sravanip <sravanip@yupptv.com>
2025-02-14 15:45:59 +0530

    added status bar color

--- app/src/main/java/com/sravani/randomusers/ui/main/MainActivity.kt
+++ app/src/main/java/com/sravani/randomusers/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sravani.randomusers
+package com.sravani.randomusers.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,39 +8,37 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.sravani.randomusers.ui.base.RandomUserNavHost
 import com.sravani.randomusers.ui.theme.RandomUsersTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RandomUsersTheme {
-                // A surface container using the 'background' color from the theme
+                SetStatusBarColor(color = MaterialTheme.colorScheme.inverseOnSurface)
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    RandomUserNavHost()
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RandomUsersTheme {
-        Greeting("Android")
+    @Composable
+    private fun SetStatusBarColor(color: Color) {
+        val systemUiController = rememberSystemUiController()
+        LaunchedEffect(key1 = color) {
+            systemUiController.setSystemBarsColor(color)
+        }
     }
+
 }
