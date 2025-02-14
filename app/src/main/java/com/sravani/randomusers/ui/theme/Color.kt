e815aae8bb39334fa7fe840b0754948ae71bacc3
sravanip <sravanip@yupptv.com>
2025-02-14 15:42:50 +0530

    added common colors, views and constants

--- app/src/main/java/com/sravani/randomusers/ui/theme/Color.kt
+++ app/src/main/java/com/sravani/randomusers/ui/theme/Color.kt
@@ -6,6 +6,9 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF0A1128)
+val PurpleGrey40 = Color(0xFFFFD700)
+val Pink40 = Color(0xFFF8F8F8)
+
+val DarkPrimaryColor = Color(0xFF0A1128)
+val SplashScreenHeaderColor = Color(0xFF0B8596)
--- app/src/main/java/com/sravani/randomusers/utils/AppConstants.kt
+++ app/src/main/java/com/sravani/randomusers/utils/AppConstants.kt
@@ -1,7 +1,7 @@
 package com.sravani.randomusers.utils
 
 object AppConstants {
-    const val APP_NAME = "RandomUser"
+    const val APP_NAME = "Random User"
     const val INITIAL_PAGE = 1
     const val PAGE_SIZE = 10
 }
